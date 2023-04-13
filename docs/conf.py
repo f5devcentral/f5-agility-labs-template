14184b51b4c756349352862cdd08507a07554ff9
russgmoore <russgmoore@users.noreply.github.com>
2023-04-13 15:11:29 -0400

    missing comma

--- docs/conf.py
+++ docs/conf.py
@@ -85,7 +85,7 @@ extensions = [
     "sphinx.ext.todo",
     "sphinx.ext.extlinks",
     "sphinx.ext.graphviz",
-    "sphinx_copybutton"
+    "sphinx_copybutton",
     "sphinxcontrib.nwdiag",
     "sphinxcontrib.blockdiag",
     "sphinx.ext.autosectionlabel",
